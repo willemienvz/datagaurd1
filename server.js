059f45e72b83e1f21a46fabe265d856ba6878443
Willemien van Tonder <willemienvz@gmail.com>
2024-09-02 14:32:54 +0200

    server int

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const jsonServer = require('json-server');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist/datagaurd')));
+
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+app.use('/api', middlewares, router);
+
+// Serve Angular app on all routes except /api
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/datagaurd/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
